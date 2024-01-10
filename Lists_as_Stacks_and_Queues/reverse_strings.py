b9d4671045deb48c048e92434f17583d187c6cd8
Dimitar Kalchev <131408520+thekalchev@users.noreply.github.com>
2024-01-10 02:16:44 +0200

    stacks

--- Lists_as_Stacks_and_Queues/reverse_strings.py
+++ Lists_as_Stacks_and_Queues/reverse_strings.py
@@ -1,1 +1,11 @@
-print("hi")
+# text = list(input())
+# while text:
+#     print(text.pop(), end='')
+
+text = list(input())
+stack = []
+
+for i in range(len(text)):
+    stack.append(text.pop())
+
+print(''.join(stack))
